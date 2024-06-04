6b662d82bd9077958394a35372471398fce06516
Ayon95 <mushfiqurrahman78@yahoo.com>
2024-06-04 00:23:14 -0400

    refactor tests

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,17 +1,19 @@
 import { expect, test } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+	await page.goto('/admin');
+});
+
 test.describe('Admin as user', () => {
 	// Tests with USER role
 	test.use({ storageState: 'playwright/.auth/user.json' });
 
 	test('shows welcome message with username', async ({ page }) => {
-		await page.goto('/admin');
 		const welcomeMessage = page.getByText(/welcome user1!/i);
 		await expect(welcomeMessage).toBeVisible();
 	});
 
 	test('shows user role privilege message', async ({ page }) => {
-		await page.goto('/admin');
 		const privilegeMessage = page.getByText(/you do not have admin privileges\./i);
 		await expect(privilegeMessage).toBeVisible();
 	});
@@ -22,7 +24,6 @@ test.describe('Admin as admin', () => {
 	test.use({ storageState: 'playwright/.auth/admin.json' });
 
 	test('shows admin role privilege message', async ({ page }) => {
-		await page.goto('/admin');
 		const privilegeMessage = page.getByText(/you have admin privileges\./i);
 		await expect(privilegeMessage).toBeVisible();
 	});
